ceb26a9496d73ebcd906cc377ea0d7cc97634602
Maxim <mihalenko.maxim21@gmail.com>
2023-01-20 14:47:27 +0300

    Change http method to delete users

--- src/repositories/AuthRepository.js
+++ src/repositories/AuthRepository.js
@@ -15,7 +15,7 @@ class AuthRepository {
     return newUser.dataValues;
   }
   async login(credentials) {
-    const date = new Date().toString();
+    const date = new Date().toLocaleString();
 
     const user = UserModel.update(
       { onlineStatus: true, lastLogin: date },
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -5,7 +5,7 @@ const { userController } = require("../controllers/index");
 const userRouter = new Router();
 
 userRouter.get("/", userController.getUsers);
-userRouter.delete("/", userController.deleteUsers);
+userRouter.put("/", userController.deleteUsers);
 userRouter.patch("/", userController.toggleUsersIsBlocked);
 
 module.exports = userRouter;
